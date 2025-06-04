3a30270e520be15afc8fb69091443dcc33e8fae7
lucaalessioo <lucaalessioo@hotmail.com>
2025-06-04 12:42:48 +0200

    Update Main.java aggiunta del prof

--- EsercizioIncapsulamentoStudente/Main.java
+++ EsercizioIncapsulamentoStudente/Main.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         Studente studente = new Studente("luca", 7);
+         ArrayList<Studente> listaStudenti = new ArrayList<>();
 
-        System.out.println("Studente : " +studente.getNome()+ " Voto " +studente.getVoto());
+        studente.stampaDettagli();
 
         // setto il voto errato
         studente.setVoto(11);
@@ -13,5 +16,11 @@ public class Main {
 
         // stampa dei dettagli
         studente.stampaDettagli();
+
+        Professore professore = new Professore("mauro" , "belli",listaStudenti);
+        professore.addStudente(studente);
+
+        System.out.println("Lista di studenti per il proff");
+        professore.stampaLista();
     }
 }
